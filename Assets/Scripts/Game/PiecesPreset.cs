8b67fc6223debe8f4879edaa51bffa257404dc74
My Mac <mymac@Mys-Mac-mini.local>
2026-02-19 23:28:46 +0800

    ss

--- Assets/Scripts/Game/PiecesPreset.cs
+++ Assets/Scripts/Game/PiecesPreset.cs
@@ -1,7 +1,32 @@
+using System.Text;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Pieces/PiecePreset")]
 public class PiecesPreset : ScriptableObject
 {
     public Piece[] Pieces;
+
+    [ContextMenu("Log Pieces")]
+    public void LogAllPieces()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine("ShapeID\tLocalX\tLocalY");
+
+        foreach (Piece piece in Pieces)
+        {
+            string shapeID = piece.ID.ToString();
+
+            foreach (Transform child in piece.transform)
+            {
+                Vector3 p = child.localPosition;
+
+                sb.AppendLine($"{shapeID}\t{p.x}\t{p.z}");
+            }
+        }
+
+        GUIUtility.systemCopyBuffer = sb.ToString();
+        Debug.Log(sb);
+        Debug.Log("Copied to clipboard!");
+    }
 }
